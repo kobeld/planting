e09b6062c3683991bcc7119a7abb738672328206
kobeld <kobeld@163.com>
2011-07-30 23:06:56 +0800

    initialize project

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3-ruby', :require => "sqlite3"
+gem 'haml'
+gem 'haml-rails'
+gem 'compass'
+gem 'jquery-rails'
+gem 'httparty'
+
+group :development do
+  gem 'annotate'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planting::Application.config.secret_token = 'd6aa2b6304212630f52ecc17fe2ef5ac45814cc05f5f5207d8e6c91315e1bc5f7505ad171c121f4622cd72253c56f627b15732db25fc9517c79c1ab92a490dd0'
