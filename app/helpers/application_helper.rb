9c5779a13925b7c249dadf892397b12f47cea40b
kobeld <kobeld@163.com>
2011-08-21 15:57:50 +0800

    finished basic info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+#encoding: UTF-8
 module ApplicationHelper
+  
+  def language_selecotr
+    if I18n.locale == :en
+      link_to "Chinese", url_for(:locale => 'cn')
+    else
+      link_to "英 语", url_for(:locale => 'en')
+    end
+  end
+  
 end
